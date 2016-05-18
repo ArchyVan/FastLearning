a6e6639f9bec6401f620f67d1160512b94d7177e
ArchyVan <2440938213@qq.com>
2016-05-18 22:11:13 +0800

    UIImage 扩展压缩方法

---
+++ UIImage+ImageCompress.h
@@ -0,0 +1,9 @@
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (ImageCompress)
+
+- (UIImage *)compressImage:(UIImage *)sourceImage toTargetWidth:(CGFloat)targetWidth {
+    CGSize imageSize = sourceImage.size;
+    
+@end
