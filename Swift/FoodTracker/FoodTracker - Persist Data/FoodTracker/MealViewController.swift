45d4c007501e27ab89bb189ada33a1d56196ac54
fanjingqi <fanjingqi@bearead.cn>
2016-09-21 15:15:55 +0800

    Some Syntax modification

--- Swift/FoodTracker/FoodTracker - Persist Data/FoodTracker/MealViewController.swift
+++ Swift/FoodTracker/FoodTracker - Persist Data/FoodTracker/MealViewController.swift
@@ -98,7 +98,7 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     
     // This method lets you configure a view controller before it's presented.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if saveButton === sender {
+        if saveButton.isEqual(sender) {
             let name = nameTextField.text ?? ""
             let photo = photoImageView.image
             let rating = ratingControl.rating
